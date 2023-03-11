7f76f9f947d895368b7b0468f47f12539475897d
AdilJahouh <adil@microlab.nl>
2023-03-11 18:58:47 +0100

    added final changes, now names instead of emails

--- flaskAPI.py
+++ flaskAPI.py
@@ -6,21 +6,35 @@ app = Flask(__name__)
 # Sample data, full preprocessing of dataframe should be done once in preprocesing.py
 
 
-def preprocessMailData(sampleSize=100, From=['Sven.Flecha@gastech.com.kronos'], width=4, data=None, blacklist=[]):
+def preprocessMailData(sampleSize=100, From=['Sven Flecha'], width=4, data=None, blacklist=[]):
     emailData = pd.read_csv('data/emailheaders.csv', encoding='cp1252')
-    # only get the mails from the selected senders
-    subset = emailData[emailData['From'].isin(From)]
+    # remove whitespaces, there are a lot in "To"
+    emailData['From'] = emailData['From'].str.strip()
+    # this is REALLY inefficient but we dont know how we want to preprocess everything yet..
+    personalData = pd.read_excel(
+        'data/EmployeeRecords.xlsx')[['EmailAddress', 'FirstName', 'LastName']]
+    personalData['FullName'] = personalData['FirstName'] + \
+        ' ' + personalData['LastName']
+    personalDataClean = personalData[['EmailAddress', 'FullName']]
+    nameToMailMapper = pd.Series(
+        personalDataClean.FullName.values, index=personalDataClean.EmailAddress).to_dict()
+    mappedMails = emailData.replace(
+        {'From': nameToMailMapper})
+    subset = mappedMails[mappedMails['From'].isin(From)]
     if subset.empty:
         print(
             f"Stopped at width {width} because no data was found for mailers {From}")
         return [['From', 'To', 'Weight']] + data
     sample = subset.sample(sampleSize)
     # get every specific recipient as a separate row
-    listifiedEmailData = sample.assign(To=emailData.To.str.split(','))
+    listifiedEmailData = sample.assign(To=sample.To.str.split(','))
     explodedEmailData = listifiedEmailData.explode('To').reset_index(drop=True)
-    # remove the mails that were sent to the same person
-    removedCycle = explodedEmailData[explodedEmailData['From']
-                                     != explodedEmailData['To']]
+    explodedEmailData['To'] = explodedEmailData['To'].str.strip()
+    replacedTo = explodedEmailData.replace(
+        {'To': nameToMailMapper})
+    # remove the mails that were sent from and to the same person
+    removedCycle = replacedTo[replacedTo['From']
+                              != replacedTo['To']]
     sankeyFormatDf = removedCycle.groupby(
         ['From', 'To'], as_index=False)['Subject'].count()
     sankeyFormatDf.rename(columns={'Subject': 'Weight'}, inplace=True)
@@ -44,7 +58,7 @@ def preprocessMailData(sampleSize=100, From=['Sven.Flecha@gastech.com.kronos'], 
 
 
 sankeyData = preprocessMailData(
-    sampleSize=3, From=['Kanon.Herrero@gastech.com.kronos', 'Sven.Flecha@gastech.com.kronos'], width=5)
+    sampleSize=10, From=['Kanon Herrero', 'Sven Flecha'], width=3)
 # Route for creating a new task
 
 
