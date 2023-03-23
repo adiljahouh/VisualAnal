2c6fc90b7d3298aac4900c7a2f62422b276e99bd
AdilJahouh <a_jahouh@hotmail.com>
2023-03-23 13:49:57 +0100

    linter

--- frontend/src/Treemap/Components/NewsModal.js
+++ frontend/src/Treemap/Components/NewsModal.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import Popup from 'reactjs-popup';
-import '../react_news_cards.css';
-import ReactNewsCard from './ReactNewsCard';
-import SentimentSlider from './SentimentSlider';
-import Stack from '@mui/material/Stack';
+import React, { useState } from "react";
+import Popup from "reactjs-popup";
+import "../react_news_cards.css";
+import ReactNewsCard from "./ReactNewsCard";
+import SentimentSlider from "./SentimentSlider";
+import Stack from "@mui/material/Stack";
 
-const NewsModal = ( {open, setOpen, cluster} ) => {
-/*
+const NewsModal = ({ open, setOpen, cluster }) => {
+  /*
 A modal containing news articles. The news articles "live" in ReactNewsCard 
 components. Whether or not this modal is open depends op open.
 
@@ -17,13 +17,23 @@ cluster: cluster_id specifying which cluster to show articles from
   const [sentimentRange, setSentimentRange] = useState([30, 67]);
 
   return (
-    <Popup open={open} closeOnDocumentClick={false} onClose={() => setOpen(false)}>
-        <Stack direction="row" spacing={1} alignItems="center">
-          <SentimentSlider sentimentRange={sentimentRange} setSentimentRange={setSentimentRange}/>
-          <div/><div/><div/><div/>
-          <button className='close-button' onClick={() => setOpen(false)}/> 
-        </Stack> 
-        <ReactNewsCard cluster={cluster} sentimentRange={sentimentRange}/>           
+    <Popup
+      open={open}
+      closeOnDocumentClick={false}
+      onClose={() => setOpen(false)}
+    >
+      <Stack direction="row" spacing={1} alignItems="center">
+        <SentimentSlider
+          sentimentRange={sentimentRange}
+          setSentimentRange={setSentimentRange}
+        />
+        <div />
+        <div />
+        <div />
+        <div />
+        <button className="close-button" onClick={() => setOpen(false)} />
+      </Stack>
+      <ReactNewsCard cluster={cluster} sentimentRange={sentimentRange} />
     </Popup>
   );
 };
